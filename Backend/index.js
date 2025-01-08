81e68b05389df5bbe1c33f56ea04e0c959476723
Tushar Gour <109131704+tushar-gour@users.noreply.github.com>
2025-01-08 23:17:27 +0530

    Create index.js

---
+++ Backend/index.js
@@ -0,0 +1,13 @@
+import { app } from "./src/app.js";
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {
+    console.log(`server running at port : ${PORT}`);
+});
+
+// App error event handling
+app.on("error", (error) => {
+    console.log("App error: ", error);
+    throw error;
+});
