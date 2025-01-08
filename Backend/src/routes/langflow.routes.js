0dc708751cac51e21521f5e69cd1d656b73fe1e2
Tushar Gour <109131704+tushar-gour@users.noreply.github.com>
2025-01-09 00:04:24 +0530

    Update langflow.routes.js

--- Backend/src/routes/langflow.routes.js
+++ Backend/src/routes/langflow.routes.js
@@ -4,6 +4,6 @@ import fetchAnalytics from "../controllers/langflow.controller.js";
 const langFlowRoute = Router();
 
 // route to hit langflow agent for analytics report
-langFlowRoute.route("/:analytics").get(fetchAnalytics);
+langFlowRoute.route("/analytics").post(fetchAnalytics);
 
 export default langFlowRoute;
