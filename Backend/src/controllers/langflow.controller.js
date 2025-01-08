8e6f876d1bc8c071fc04febf84b6b1fb44fa3bde
Tushar Gour <109131704+tushar-gour@users.noreply.github.com>
2025-01-09 00:03:49 +0530

    Update langflow.controller.js

--- Backend/src/controllers/langflow.controller.js
+++ Backend/src/controllers/langflow.controller.js
@@ -1,36 +1,14 @@
-import LangflowClient from "../services/langflowclient.js";
-
-const langflowClient = new LangflowClient();
+import langflowMain from "../services/langflowclient.js";
 
 const fetchAnalytics = async (req, res) => {
     try {
-        const {
-            inputValue,
-            inputType = "text",
-            outputType = "chat",
-        } = req.body;
+        const { inputValue } = req.body;
 
-        const langflowID = process.env.LANGFLOW_ID;
-        const flowID = process.env.FLOW_ID;
-
-        if (!process.env.API_TOKEN) {
-            return res.status(500).json({ message: "Missing API Token" });
-        }
-
-        const endPoint = `/lf/${langflowID}/api/v1/run/${flowID}`;
-        const body = {
-            input_value: inputValue,
-            input_type: inputType,
-            output_type: outputType,
-            tweaks: {},
-        };
-
-        const response = await langflowClient.post(endPoint, body);
-
-        res.status(200).json({ message: response });
+        const response = await langflowMain(inputValue);
+        return res.status(200).json({ message: response });
     } catch (error) {
         console.error("Error in fetchAnalytics:", error.message);
-        res.status(500).json({ message: error.message });
+        return res.status(500).json({ message: error.message });
     }
 };
 
