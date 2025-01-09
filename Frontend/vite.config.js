748c9b76e89ca4b536e41d37b3806d6a1840b316
tushar-gour <tushargour004@gmail.com>
2025-01-09 16:12:48 +0530

    updated url

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://github.com/tushar-gour/social-analytics/Frontend",
+  base: "social-analytics-mu.vercel.app",
 });
