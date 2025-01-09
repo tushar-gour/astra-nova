b5aeeb8ba6feb45ca4981cc1c4b9f86cfe54924d
shriram tiwari <shriramt.124@gmail.com>
2025-01-09 10:00:02 +0000

    github link added for soruce code

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -133,7 +133,7 @@ const App = () => {
                   className="flex items-center gap-2 px-4 py-2 rounded-lg bg-white/5 hover:bg-white/10 transition-all"
                 >
                   <Github className="w-5 h-5" />
-                  <span>GitHub</span>
+                  <span><a href="https://github.com/tushar-gour/social-analytics" >GitHub</a></span>
                 </a>
                 <a 
                   href="https://youtube.com/your-demo" 
