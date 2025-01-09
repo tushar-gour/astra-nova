13f504e7b564287360512e5178e3a70cc10b7830
tushar-gour <tushargour004@gmail.com>
2025-01-09 16:23:26 +0530

    update

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
+  esbuild: {
+    loader: "jsx", // or 'js'
+    include: /\.jsx?$/, // Ensure JavSAcript files are correctly included
+    exclude: [], // No exclusions here, unless you need to exclude specific files
+  },
   plugins: [react()],
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".jsx"],
+  },
+  server: {
+    hmr: {
+      overlay: false, // Disable the HMR error overlay in development
+    },
+  },
 });
