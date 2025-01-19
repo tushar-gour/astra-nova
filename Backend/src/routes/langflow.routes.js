740e0c7f6461792bb297bc7773bd93a8a3b95ff3
tushar-gour <tushargour004@gmail.com>
2025-01-19 07:40:55 +0530

    updated

--- Backend/src/routes/langflow.routes.js
+++ Backend/src/routes/langflow.routes.js
@@ -4,6 +4,6 @@ import fetchAnalytics from "../controllers/langflow.controller.js";
 const langFlowRoute = Router();
 
 // route to hit langflow agent for analytics report
-langFlowRoute.route("/analytics").post(fetchAnalytics);
+langFlowRoute.route("/astrology").post(fetchAnalytics);
 
 export default langFlowRoute;
