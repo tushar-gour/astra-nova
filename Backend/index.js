51ea6529525e2c006644ea3ed9137370a9f98410
tushar-gour <tushargour004@gmail.com>
2025-01-19 08:37:23 +0530

    updateseeeeeeeeeeeeeeeeeeeeee

--- Backend/index.js
+++ Backend/index.js
@@ -2,13 +2,20 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import cors from "cors";
-
 import app from "./src/app.js";
 
 const PORT = process.env.PORT || 8000;
 
-// Use CORS middleware in your app
-app.use(cors({ origin: "*" })); // Allow requests from all origins
+app.use(cors({
+    origin: "*", // Client origin
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true
+}));
+
+// Other middleware and routes come after
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.listen(PORT, () => {
     console.log(`Server running at port: ${PORT}`);
