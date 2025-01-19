f16c15c0d9b54c4722823631422c750f262bc69b
tushar-gour <tushargour004@gmail.com>
2025-01-19 07:45:10 +0530

    update

--- Backend/src/utils/horoscope.js
+++ Backend/src/utils/horoscope.js
@@ -1,7 +1,8 @@
 import asyncHandler from "express-async-handler";
+import langflowMain from "../services/langflowclient";
 
-const getHoroscope = asyncHandler(async () => {
-    
+const getHoroscope = asyncHandler(async (dob, birthTime) => {
+   return await langflowMain();
 });
 
 export default getHoroscope;
